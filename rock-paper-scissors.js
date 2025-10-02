562f24eafac79203914ccb992a013eb4a6d7a902
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-10-02 16:06:11 -0700

    Add results text that updates based on winner

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -19,35 +19,37 @@ function getHumanChoice(){
 let humanScore = 0;
 let computerScore = 0;
 
+//get ref to results text
+const resultsText = document.getElementById("resultsText");
 
 
 //Logic to play a single round
 function playRound(humanChoice, computerChoice){
     const normalizedHumanChoice = humanChoice.toLowerCase();
     if (normalizedHumanChoice === "rock" && computerChoice === "rock"){
-        console.log("TIE! TWO ROCKS CLASH!");
+        resultsText.textContent = ("TIE! TWO ROCKS CLASH!");
     }else if(normalizedHumanChoice === "rock" && computerChoice === "paper"){
-        console.log("YOU LOSE! YOUR ROCK IS COVERED BY PAPER!");
+        resultsText.textContent = ("YOU LOSE! YOUR ROCK IS COVERED BY PAPER!");
         computerScore++;
     }else if(normalizedHumanChoice === "rock" && computerChoice === "scissors"){
-        console.log("YOU WIN! YOUR ROCK SMASHES SCISSORS!");
+        resultsText.textContent = ("YOU WIN! YOUR ROCK SMASHES SCISSORS!");
         humanScore++;
     }else if(normalizedHumanChoice === "paper" && computerChoice === "rock"){
-        console.log("YOU WIN! YOUR PAPER COVERS ROCK!");
+        resultsText.textContent = ("YOU WIN! YOUR PAPER COVERS ROCK!");
         humanScore++;
     }else if(normalizedHumanChoice === "paper" && computerChoice === "paper"){
-        console.log("TIE! TWO PAPERS FOLD!");
+        resultsText.textContent = ("TIE! TWO PAPERS FOLD!");
     }else if(normalizedHumanChoice === "paper" && computerChoice === "scissors"){
-        console.log("YOU LOSE! YOUR PAPER IS SLICED BY SCISSORS!");
+        resultsText.textContent = ("YOU LOSE! YOUR PAPER IS SLICED BY SCISSORS!");
         computerScore++;
     }else if(normalizedHumanChoice === "scissors" && computerChoice === "rock"){
-        console.log("YOU LOSE! YOUR SCISSORS ARE SMASHED BY ROCK!");
+        resultsText.textContent = ("YOU LOSE! YOUR SCISSORS ARE SMASHED BY ROCK!");
         computerScore++;
     }else if(normalizedHumanChoice === "scissors" && computerChoice === "paper"){
-        console.log("YOU WIN! YOUR SCISSORS SLICE PAPER!");
+        resultsText.textContent = ("YOU WIN! YOUR SCISSORS SLICE PAPER!");
         humanScore++;
     }else if(normalizedHumanChoice === "scissors" && computerChoice === "scissors"){
-        console.log("TIE! TWO SCISSOR LOCK BLADES!");
+        resultsText.textContent = ("TIE! TWO SCISSOR LOCK BLADES!");
     }
 }
 
@@ -73,7 +75,7 @@ function playGame(){
         
         playRound(humanSelection, computerSelection);
 
-        console.log(`The score is You ${humanScore} - Computer ${computerScore}`);
+        resultsText.textContent = (`The score is You ${humanScore} - Computer ${computerScore}`);
     }
 
     if (humanScore > computerScore){
